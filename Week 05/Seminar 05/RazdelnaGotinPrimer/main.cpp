e3b836ba40bc6f7866ace656259ff2c49250d9eb
Serhan Chavdarliev <serhi1334@gmail.com>
2025-03-26 01:02:38 +0200

    fixed cyrilic BUG typed in ENGLISH

--- Week 05/Seminar 05/RazdelnaGotinPrimer/main.cpp
+++ Week 05/Seminar 05/RazdelnaGotinPrimer/main.cpp
@@ -2,8 +2,12 @@
 #include <iostream>
 //#include "h1.h"
 #include "h1.cpp"
-//!!!!! ме лнфе дю хлю 2 дейкюпюжхх мю едмю х языю тсмйжхъ б CPP тюик
-//! хлюл дейкюпхъ мю F() бзб MAIN.CPP х бзб h3.cpp (йюрн INCLUD-VAME h3.h)
+//!!! NE MOJE DA IMA 2 Implementacii NA FUNKCIQ S EDNO I SUSHTO IME V razlichni cpp failove
+// chupi se edinnata kompilacionna edinica trqbva da se sloji static ili anonimen namespace
+
+//! IMAM IMPLEMENTACIQ мю f() VUV MAIN.CPP I VUV h3.cpp(v h3.cpp idva kato includa-vame h3.h)
+//!! Moje da slojim samo deklariq v h3.h i da implementirame f() v h3.cpp,
+// togava shte imame f() v main.cpp i f() v h3.cpp otnovo i shte grumne s LINKING greshka
 void f() {
 	std::cout << "obj";
 }
