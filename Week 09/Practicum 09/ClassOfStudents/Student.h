06c5fc988cb80b4c9ffeb2848b70369a1bed9f74
Serhan Chavdarliev <67830778+darimachine@users.noreply.github.com>
2025-04-16 23:50:53 +0300

    Update Student.h

--- Week 09/Practicum 09/ClassOfStudents/Student.h
+++ Week 09/Practicum 09/ClassOfStudents/Student.h
@@ -6,7 +6,6 @@ class Student
 	size_t age=0;
 
 public:
-	explicit Student(const char* name);
 	Student(const MyString& name, size_t age);
 	Student(MyString&& name, size_t age);
 	void print() const;
