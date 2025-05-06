f8a10bc191642e30fd9a1d4b6b3e8ab0a57b9916
Serhan Chavdarliev <serhi1334@gmail.com>
2025-05-07 00:32:41 +0300

    addd

---
+++ Week 12/SimpleVisitor-FileSystem/main.cpp
@@ -0,0 +1,72 @@
+
+
+#include <iostream>
+#include "MyString.h"
+
+class Visitor {
+public:
+
+    virtual void visitFile(File *) = 0;
+    virtual void visitDirectory(Directory*) = 0;
+    virtual ~Visitor() = 0;
+};
+class FileSystem {
+protected:
+    MyString name;
+public:
+    FileSystem(const MyString& name) : name(name) {
+
+    }
+    const MyString& getName() const {
+        return name;
+    }
+    virtual void accept(Visitor* v)=0;
+    virtual ~FileSystem() = 0;
+};
+
+class File : public FileSystem {
+    
+    void accept(Visitor* v) override
+    {
+        v->visitFile(this);
+    }
+};
+class Directory : public FileSystem {
+public:
+    Directory(const MyString& name) : FileSystem(name) {
+        
+    }
+    FileSystem** children;
+    int size = 0;
+    int capacity = 8;
+
+    void accept(Visitor* v) override {
+        v->visitDirectory(this);
+    }
+
+};
+class FileSystemVisitor : public Visitor {
+
+public:
+    void visitFile(File* f) override {
+        cout << f->getName();
+    }
+
+    void visitDirectory(Directory* dir) override {
+        cout << dir->getName();
+        for (int i = 0; i < dir->size; i++)
+        {
+            dir->children[i]->accept(this);
+        }
+    }
+};
+int main()
+{
+    FileSystem* f = new Directory("src/");
+    Visitor* v = new FileSystemVisitor();
+    f->accept(v);
+    delete v;
+    delete f;
+}
+
+
